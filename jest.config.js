116d9b4abe8639a44d00f41396d50ac1014362e3
jseto <jsetojseto@gmail.com>
2023-01-18 16:35:06 +0700

    fix: npm update

--- jest.config.js
+++ jest.config.js
@@ -7,13 +7,8 @@ const pathsToIgnore = [
 ];
 
 module.exports = {
-	globals: {
-		'ts-jest': { // configuramos para que Jest use typescript
-			'tsconfig': 'tsconfig.json', // usar la configuración estandart de TS
-		}
-	},
 	transform: { // usar TS para extensiones de fichero ts i tsx
-		"^.+\\.(ts|tsx)$": "ts-jest"
+		"^.+\\.(ts|tsx)$": ["ts-jest", { 'tsconfig': 'tsconfig.json' }]
 	},
 	testMatch: [ // ejecutar tests en los ficheros que contengan ".spec" justo antes de extensiones ts, tsx, js, jsx y se encuentren en una carpeta test
 		"**/*.spec.+(ts|tsx|js|jsx)"
