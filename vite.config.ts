34d4320950c6c7aaf1ac4b025fc957245a852a55
jseto <jsetojseto@gmail.com>
2024-02-08 17:06:52 +0700

    feat: esm and cjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   test: {
@@ -7,4 +9,15 @@ export default defineConfig({
 		setupFiles: './add-ons/setup-tests.ts',
 		exclude: ['**/node_modules', '**/dist', '.idea', '.git', '.cache','**/lib', '**/out'],
 	},
+	build: {
+		lib: {
+			entry: resolve( __dirname, 'src/index.ts' ),
+			name: 'entropic-bond-localize-react',
+			fileName: 'entropic-bond-localize-react'
+		},
+		"outDir": "lib",
+	},
+	plugins: [
+		dts()
+	]
 })
